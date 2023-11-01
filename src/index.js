f5ab883728982fe685970c0a3deef33e3263e1cc
unknown <lstradiot15@gmail.com>
2023-11-01 15:58:06 -0300

    Added some folders and files for the initial setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Routes from './Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
