ebac0f6c8061986111484d8fa19df9578ceb8dd1
unknown <lstradiot15@gmail.com>
2023-11-06 13:09:18 -0300

    Change the name on the button function

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './button.module.css';
 
-const Button = ({ text, type, clickBtn }) => {
+const Button = ({ text, type, onClick }) => {
   return type === 'submit' ? (
-    <button onClick={clickBtn} className={`${styles.submitBtn} ${styles.button}`}>
+    <button onClick={onClick} className={`${styles.submitBtn} ${styles.button}`}>
       {text}
     </button>
   ) : (
     <button
-      onClick={clickBtn}
+      onClick={onClick}
       className={
         type === 'cancel'
           ? `${styles.cancelBtn} ${styles.button}`
