8740ca6e89738a1472d72b73af9dfd3e8b7a8a1a
unknown <lstradiot15@gmail.com>
2023-11-05 23:26:46 -0300

    Finish for now. Once I merged the shared buttons I'll should add to the modal

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -1,17 +1,9 @@
 import React, { useEffect } from 'react';
 import styles from './modal.module.css';
 
-const Modal = ({
-  title,
-  desc,
-  isOpen,
-  img,
-  confirmBtn,
-  denyBtn,
-  chooseModal,
-  close,
-  clickFunction
-}) => {
+/**This component is used by passing the properties of title for the title, desc for the description, isOpen to open the modal, confirmBtn for the confirm or accept button, denyBtn for the cancel or exit button, clickFunction, which is what suits it to indicate to the confirm button the action you want to perform and chooseModal to determine whether to use a modal that requires a confirmation or a modal that shows a text that disappears after 5 seconds */
+
+const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, clickFunction }) => {
   useEffect(() => {
     let timer;
     if (!chooseModal && isOpen) {
@@ -29,8 +21,10 @@ const Modal = ({
     chooseModal ? (
       <div className={styles.container}>
         <div className={styles.subContainer}>
-          <div>{img}</div>
-          <div className={styles.title}>{title}</div>
+          <div className={styles.logoContainer}>
+            <img className={styles.logoImg} src="assets/logo.png" alt="logo-subastodo" />
+          </div>
+          <div className={styles.title}>{title.toUpperCase()}</div>
           <div className={styles.subTitle}>{desc}</div>
           <button onClick={clickFunction}>{confirmBtn}</button>
           <button onClick={close}>{denyBtn}</button>
@@ -38,10 +32,11 @@ const Modal = ({
       </div>
     ) : (
       <>
-        <div>
-          <button onClick={close}>X</button>
-          <div>{title}</div>
-          <div>{desc}</div>
+        <div className={styles.containerSelfClose}>
+          <div className={styles.subContainerSelfClose}>
+            <div className={styles.descSelfClose}>{desc}</div>
+            <button onClick={close}>X</button>
+          </div>
         </div>
       </>
     )
