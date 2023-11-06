142340939af6338079d5cf4a1220c0db90c99406
unknown <lstradiot15@gmail.com>
2023-11-06 14:01:13 -0300

    Added shared buttons to the modal

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import styles from './modal.module.css';
+import Button from '../Button';
 
 /**This component is used by passing the properties of title for the title, desc for the description, isOpen to open the modal, confirmBtn for the confirm or accept button, denyBtn for the cancel or exit button, clickFunction, which is what suits it to indicate to the confirm button the action you want to perform and chooseModal to determine whether to use a modal that requires a confirmation or a modal that shows a text that disappears after 5 seconds */
 
-const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, onClick }) => {
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  confirmBtn,
+  denyBtn,
+  chooseModal,
+  close,
+  onClick
+}) => {
   const [isFadingOut, setIsFadingOut] = useState(false);
 
   useEffect(() => {
@@ -31,17 +41,17 @@ const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, o
             <img className={styles.logoImg} src="assets/logo.png" alt="logo-subastodo" />
           </div>
           <div className={styles.title}>{title.toUpperCase()}</div>
-          <div className={styles.subTitle}>{desc}</div>
-          <button onClick={onClick}>{confirmBtn}</button>
-          <button onClick={close}>{denyBtn}</button>
+          <div className={styles.subTitle}>{description}</div>
+          <Button type="submit" text={confirmBtn} onClick={onClick} />
+          <Button type="cancel" text={denyBtn} onClick={close} />
         </div>
       </div>
     ) : (
       <>
         <div className={styles.containerSelfClose}>
           <div className={!isFadingOut ? styles.subContainerSelfClose : styles.subContainerOpen}>
-            <div className={styles.descSelfClose}>{desc}</div>
-            <button onClick={close}>X</button>
+            <div className={styles.descriptionSelfClose}>{description}</div>
+            <Button type="x" text={'X'} onClick={close} />
           </div>
         </div>
       </>
