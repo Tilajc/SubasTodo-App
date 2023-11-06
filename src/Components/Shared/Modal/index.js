f8d42462a1e89995ea39dba2a3b7061ffd5329c0
unknown <lstradiot15@gmail.com>
2023-11-06 13:04:43 -0300

    Added keyframe for the ending of the transition on the modal that is not for confirmation

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './modal.module.css';
 
 /**This component is used by passing the properties of title for the title, desc for the description, isOpen to open the modal, confirmBtn for the confirm or accept button, denyBtn for the cancel or exit button, clickFunction, which is what suits it to indicate to the confirm button the action you want to perform and chooseModal to determine whether to use a modal that requires a confirmation or a modal that shows a text that disappears after 5 seconds */
 
-const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, clickFunction }) => {
+const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, onClick }) => {
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
   useEffect(() => {
     let timer;
     if (!chooseModal && isOpen) {
       timer = setTimeout(() => {
-        close();
+        setIsFadingOut(false);
+        setTimeout(() => {
+          close();
+        }, 500);
       }, 5000);
     }
+    setIsFadingOut(true);
 
     return () => {
       clearTimeout(timer);
@@ -26,14 +32,14 @@ const Modal = ({ title, desc, isOpen, confirmBtn, denyBtn, chooseModal, close, c
           </div>
           <div className={styles.title}>{title.toUpperCase()}</div>
           <div className={styles.subTitle}>{desc}</div>
-          <button onClick={clickFunction}>{confirmBtn}</button>
+          <button onClick={onClick}>{confirmBtn}</button>
           <button onClick={close}>{denyBtn}</button>
         </div>
       </div>
     ) : (
       <>
         <div className={styles.containerSelfClose}>
-          <div className={styles.subContainerSelfClose}>
+          <div className={!isFadingOut ? styles.subContainerSelfClose : styles.subContainerOpen}>
             <div className={styles.descSelfClose}>{desc}</div>
             <button onClick={close}>X</button>
           </div>
