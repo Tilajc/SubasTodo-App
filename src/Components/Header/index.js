ff790325525c081d517dcb585272370dc3c02583
Victoria Negrotto <vnegrotto19@gmail.com>
2023-11-08 14:59:44 -0300

    ST-33: start with header component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import styles from './header.module.css';
 
 const Header = () => {
+  const pepito = 'Hola Mundo';
   return (
     <div>
-      <Link to="/">Header</Link>
+      <header className={styles.header}>{pepito}</header>
+      <h1></h1>
     </div>
   );
 };
