b3efbb6b4063a1f97ab9fe1ee3d4921c698aa71f
Victoria Negrotto <vnegrotto19@gmail.com>
2023-11-08 15:23:02 -0300

    ST-33: work on header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './header.module.css';
 
 const Header = () => {
-  const pepito = 'Hola Mundo';
+  const pepito = 'SubasTodo';
   return (
     <div>
       <header className={styles.header}>{pepito}</header>
-      <h1></h1>
+      <h1>SubasTodo</h1>
+      <img src="assets/logo.png" alt="" className={styles.header} />
+      <button></button>
     </div>
   );
 };
